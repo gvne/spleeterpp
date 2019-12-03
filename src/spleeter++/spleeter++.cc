f20cdc794f366894d74b2cf2d32b6daa8ca7a5a1
gvne <guillaume.vincke@gmail.com>
2019-12-03 22:10:15 +0100

    Add the 4 and 5 stems function

--- src/spleeter++/spleeter++.cc
+++ src/spleeter++/spleeter++.cc
@@ -1,5 +1,6 @@
 #include "spleeter++/spleeter++.h"
 #include <memory>
+#include <map>
 
 #include <pybind11/embed.h>
 
@@ -41,13 +42,17 @@ void Initialize(const std::string &virtual_environment_path) {
   pybind11::exec("from spleeter.separator import Separator");
 }
 
+/// Separate multiple stems
+/// \param audio_file_path the source file path
+/// \param output_files a mapping between [expected output file name] -> [output
+/// file path]
 void Separate(const std::string &audio_file_path,
-              const std::string &vocals_path,
-              const std::string &accompaniment_path) {
+              const std::map<std::string, std::string> &output_files) {
   pybind11::exec("output_path = tempfile.mkdtemp('spleeter')");
 
   // Initialize the separator
-  pybind11::exec("separator = Separator('spleeter:2stems')");
+  auto stem_count = std::to_string(output_files.size());
+  pybind11::exec("separator = Separator('spleeter:" + stem_count + "stems')");
   pybind11::exec("source_file = '" + audio_file_path + "'");
 
   // Run it !
@@ -58,14 +63,51 @@ void Separate(const std::string &audio_file_path,
       "source_filename = os.path.splitext(os.path.basename(source_file))[0]");
   pybind11::exec(
       "output_audio_path = os.path.join(output_path, source_filename)");
-  pybind11::exec(
-      "shutil.move(os.path.join(output_audio_path, 'vocals.wav'), '" +
-      vocals_path + "')");
-  pybind11::exec("shutil.move(os.path.join(output_audio_path, "
-                 "'accompaniment.wav'), '" +
-                 accompaniment_path + "')");
+  for (auto output_file : output_files) {
+    auto source = output_file.first;
+    auto destination = output_file.second;
+    pybind11::exec("shutil.move(os.path.join(output_audio_path, '" + source +
+                   "'), '" + destination + "')");
+  }
 
   pybind11::exec("shutil.rmtree(output_path)"); // cleanup
+}
+
+void Separate(const std::string &audio_file_path,
+              const std::string &vocals_path,
+              const std::string &accompaniment_path) {
+  Separate(audio_file_path, {
+    { "vocals.wav", vocals_path },
+    { "accompaniment.wav", accompaniment_path }
+  });
+}
+
+void Separate(const std::string& audio_file_path,
+              const std::string& vocals_path,
+              const std::string& drums_path,
+              const std::string& bass_path,
+              const std::string& other_path) {
+  Separate(audio_file_path, {
+    { "vocals.wav", vocals_path },
+    { "drums.wav", drums_path },
+    { "bass.wav", bass_path },
+    { "other.wav", other_path }
+  });
+}
+
+void Separate(const std::string& audio_file_path,
+              const std::string& vocals_path,
+              const std::string& drums_path,
+              const std::string& bass_path,
+              const std::string& piano_path,
+              const std::string& other_path) {
+  Separate(audio_file_path, {
+    { "vocals.wav", vocals_path },
+    { "drums.wav", drums_path },
+    { "bass.wav", bass_path },
+    { "piano.wav", piano_path },
+    { "other.wav", other_path }
+  });
 }
 
 } // namespace spleeter
--- test/spleeter++/test_basic.cc
+++ test/spleeter++/test_basic.cc
@@ -1,7 +1,30 @@
 #include <gtest/gtest.h>
 #include "spleeter++/spleeter++.h"
 
-TEST(Spleeter, Basic) {
-  spleeter::Initialize(std::string(SPLEETER_ENV));
+// ----------
+// TODO: this should be moved to another file.
+class Environment : public ::testing::Environment {
+ public:
+  virtual ~Environment() {}
+
+  void SetUp() override {
+    spleeter::Initialize(std::string(SPLEETER_ENV));
+  }
+};
+::testing::Environment* const foo_env = ::testing::AddGlobalTestEnvironment(new Environment);
+// ----------
+
+TEST(Spleeter, TwoStems) {
   spleeter::Separate(std::string(TEST_FILE_PATH), "vocals.wav", "other.wav");
 }
+
+TEST(Spleeter, FourStems) {
+  spleeter::Separate(std::string(TEST_FILE_PATH), "vocals.wav", "drums.wav",
+                     "bass.wav", "other.wav");
+}
+
+TEST(Spleeter, FiveStems) {
+  spleeter::Separate(std::string(TEST_FILE_PATH), "vocals.wav", "drums.wav",
+                     "bass.wav", "piano.wav", "other.wav");
+}
+
