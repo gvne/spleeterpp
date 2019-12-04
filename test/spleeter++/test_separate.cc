3ac95214ec770e655987fd6f29a83d970ead75b5
gvne <guillaume.vincke@gmail.com>
2019-12-04 20:19:55 +0100

    Reorganize the tests

--- test/spleeter++/test_separate.cc
+++ test/spleeter++/test_separate.cc
@@ -1,18 +1,6 @@
 #include <gtest/gtest.h>
 #include "spleeter++/spleeter++.h"
 
-// ----------
-// TODO: this should be moved to another file.
-class Environment : public ::testing::Environment {
- public:
-  virtual ~Environment() {}
-
-  void SetUp() override {
-    spleeter::Initialize(std::string(SPLEETER_ENV));
-  }
-};
-::testing::Environment* const foo_env = ::testing::AddGlobalTestEnvironment(new Environment);
-// ----------
 
 TEST(Spleeter, TwoStems) {
   spleeter::Separate(std::string(TEST_FILE_PATH), "vocals.wav", "other.wav");
@@ -27,4 +15,3 @@ TEST(Spleeter, FiveStems) {
   spleeter::Separate(std::string(TEST_FILE_PATH), "vocals.wav", "drums.wav",
                      "bass.wav", "piano.wav", "other.wav");
 }
-
