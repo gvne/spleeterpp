a8c889e3df7f44aef00cf0670111ba0bafdc0388
gvne <guillaume.vincke@gmail.com>
2019-12-04 21:50:28 +0100

    Update cmake system to avoid building the example by default and changing the tests output directory

--- test/spleeter++/test_separate.cc
+++ test/spleeter++/test_separate.cc
@@ -3,15 +3,30 @@
 
 
 TEST(Spleeter, TwoStems) {
-  spleeter::Separate(std::string(TEST_FILE_PATH), "vocals.wav", "other.wav");
+  spleeter::Separate(
+    std::string(TEST_FILE_PATH),
+    std::string(OUTPUT_DIR) + "/2stems_vocals.wav",
+    std::string(OUTPUT_DIR) + "/2stems_other.wav"
+  );
 }
 
 TEST(Spleeter, FourStems) {
-  spleeter::Separate(std::string(TEST_FILE_PATH), "vocals.wav", "drums.wav",
-                     "bass.wav", "other.wav");
+  spleeter::Separate(
+    std::string(TEST_FILE_PATH),
+    std::string(OUTPUT_DIR) + "/4stems_vocals.wav",
+    std::string(OUTPUT_DIR) + "/4stems_drums.wav",
+    std::string(OUTPUT_DIR) + "/4stems_bass.wav",
+    std::string(OUTPUT_DIR) + "/4stems_other.wav"
+  );
 }
 
 TEST(Spleeter, FiveStems) {
-  spleeter::Separate(std::string(TEST_FILE_PATH), "vocals.wav", "drums.wav",
-                     "bass.wav", "piano.wav", "other.wav");
+  spleeter::Separate(
+    std::string(TEST_FILE_PATH),
+    std::string(OUTPUT_DIR) + "/5stems_vocals.wav",
+    std::string(OUTPUT_DIR) + "/5stems_drums.wav",
+    std::string(OUTPUT_DIR) + "/5stems_bass.wav",
+    std::string(OUTPUT_DIR) + "/5stems_piano.wav",
+    std::string(OUTPUT_DIR) + "/5stems_other.wav"
+  );
 }
