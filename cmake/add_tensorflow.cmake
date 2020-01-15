30f49a7b1709db7345d5a352cd4a4e5b714d903b
gvne <guillaume.vincke@gmail.com>
2020-01-15 21:53:24 +0100

    Use prebuilt tensorflow cc if install dir not given

--- cmake/add_tensorflow.cmake
+++ cmake/add_tensorflow.cmake
@@ -1,3 +1,27 @@
+if (NOT EXISTS ${TENSORFLOW_CC_INSTALL_DIR})
+  message(STATUS "Downloading pre-built version of tensorflow_cc library")
+  if (WIN32)
+    message(FATAL_ERROR "Pre-built are not available for windows")
+  elseif(UNIX AND APPLE)
+    set(url https://github.com/gvne/spleeterpp/releases/download/tf_cc-1.14.0/tf_cc-1.14.0-osx.zip)
+  else()
+    set(url https://github.com/gvne/spleeterpp/releases/download/tf_cc-1.14.0/tf_cc-1.14.0-linux.zip)
+  endif()
+  set(zipped_lib tensorflow_cc.zip)
+  # TODO: use md5 to avoid redownloading instead of this check
+  if (NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/${zipped_lib})
+    file(DOWNLOAD ${url} ${CMAKE_CURRENT_BINARY_DIR}/${zipped_lib} SHOW_PROGRESS)
+  endif()
+  execute_process(COMMAND ${CMAKE_COMMAND} -E tar -xf ${zipped_lib}
+                  WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
+  set(TENSORFLOW_CC_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/install)
+  if (UNIX AND APPLE)  # TODO: should be included in the zip
+    execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink
+                    ${TENSORFLOW_CC_INSTALL_DIR}/bin/libtensorflow_cc.so
+                    ${TENSORFLOW_CC_INSTALL_DIR}/bin/libtensorflow_cc.so.1)
+  endif()
+endif()
+
 find_library(tensorflow_cc
   NAMES tensorflow_cc
   PATHS ${TENSORFLOW_CC_INSTALL_DIR}/bin
