7f82a95a70b8d8ab1447e3f028a14917386fec49
gvne <guillaume.vincke@gmail.com>
2020-02-09 17:35:34 +0100

    Implement the spectrogram processing in tests

--- test/spleeter/test_spleeter.cc
+++ test/spleeter/test_spleeter.cc
@@ -7,76 +7,108 @@
 
 #include "rtff/filter.h"
 
-TEST(Spleeter, RTFF) {
-  rtff::Filter filter;
-  std::error_code err;
-  auto channel_number = 1;
-  filter.Init(channel_number, 2048, 1024, rtff::fft_window::Type::Hann, err);
-  ASSERT_FALSE(err);
-
-  filter.execute = [](std::vector<std::complex<float>*> data, uint32_t size) {
-    for (uint8_t channel_idx = 0; channel_idx < data.size(); channel_idx++) {
-      auto buffer = Eigen::Map<Eigen::VectorXcf>(data[channel_idx], size);
-      buffer = Eigen::VectorXcf::Random(size);
-    }
-  };
-  rtff::AudioBuffer buffer(filter.block_size(), filter.channel_count());
-  for (auto index = 0; index < 50; index++) {
-    filter.ProcessBlock(&buffer);
-  }
+void Write(const spleeter::Waveform& data, const std::string& name) {
+  std::vector<float> vec_data(data.size());
+  std::copy(data.data(), data.data() + data.size(), vec_data.data());
+  wave::File file;
+  file.Open(std::string(OUTPUT_DIR) + "/" + name + ".wav", wave::kOut);
+  file.set_sample_rate(44100);
+  file.set_channel_number(2);
+  file.Write(vec_data);
 }
 
 TEST(Spleeter, Spectrogram) {
   std::error_code err;
   
+  // read a file
+  wave::File file;
+  std::string test_file(TEST_FILE);
+//  std::string test_file("/Users/gvne/Desktop/snipet.wav");
+  file.Open(test_file, wave::kIn);
+  auto data = file.Read(err);
+  ASSERT_FALSE(err);
+  
   // 60sec 44.1KHz
-  const auto sample_count = 44100 * 60;
+//  const auto sample_count = 44100 * 60;
+  const auto channel_number = 2;
   // stft parameters
   const auto frame_length = 4096;
   const auto half_frame_length = frame_length / 2 + 1;
   const auto frame_step = 1024;
+  
+  // initialize the rtff filter
+  rtff::Filter filter;
+  const auto block_size = 2048;
+  filter.Init(channel_number, frame_length, frame_length - frame_step, rtff::fft_window::Type::Hann, err);
+  ASSERT_FALSE(err);
+  filter.set_block_size(block_size);
+  rtff::AudioBuffer buffer(block_size, channel_number);
 
-  const auto frame_count = sample_count / frame_step;
-  Eigen::MatrixXcf left = Eigen::MatrixXcf::Random(half_frame_length, frame_count);
-  Eigen::MatrixXcf right = Eigen::MatrixXcf::Random(half_frame_length, frame_count);
+//  const auto frame_count = sample_count / frame_step;
+//  Eigen::MatrixXcf left = Eigen::MatrixXcf::Random(half_frame_length, frame_count);
+//  Eigen::MatrixXcf right = Eigen::MatrixXcf::Random(half_frame_length, frame_count);
   
   const auto separation_type = spleeter::TwoStems;
-  std::vector<tensorflow::Tensor> output;
 
   spleeter::Initialize(std::string(SPLEETER_MODELS), separation_type, err);
   ASSERT_FALSE(err);
-  
   
   // Find the right model
   auto bundle = spleeter::Registry::instance().Get(separation_type);
   ASSERT_TRUE(bundle);
   
   // Initialize the input
+  std::vector<tensorflow::Tensor> output;
   tensorflow::Tensor tf_input(
       tensorflow::DT_COMPLEX64,
-      tensorflow::TensorShape({frame_count, half_frame_length, 2}));
+      tensorflow::TensorShape({1, half_frame_length, 2}));
+  
+  filter.execute = [bundle, &tf_input, &output](std::vector<std::complex<float>*> data, uint32_t size) {
+    auto flatten = tf_input.flat<std::complex<float>>();
+    auto flatten_ptr = flatten.data();
+    std::copy(data[0], data[0] + size, flatten_ptr);
+    std::copy(data[1], data[1] + size, flatten_ptr + size);
+    
+    auto status = bundle->session->Run(
+        {{"Placeholder", tf_input}}, GetOutputNames(separation_type), {}, &output);
+    ASSERT_TRUE(status.ok());
+    
+    // Apply the output mask
+    auto mask_size = output[0].dim_size(1);
+    Eigen::Map<Eigen::VectorXf> left_mask(output[0].flat<float>().data(), mask_size);
+    Eigen::Map<Eigen::VectorXf> right_mask(output[0].flat<float>().data() + mask_size, mask_size);
 
-  // TODO: Find another way to copy the input data
-//  std::copy(input.data(), input.data() + input.size(),
-//            tf_input.matrix<float>().data());
+    Eigen::Map<Eigen::VectorXcf> left_channel(data[0], mask_size);
+    Eigen::Map<Eigen::VectorXcf> right_channel(data[1], mask_size);
+    
+    left_channel.array() *= left_mask.array();
+    right_channel.array() *= right_mask.array();
+    
+    output.clear();
+  };
+  
+  // Run each frames
+  auto multichannel_buffer_size = block_size * channel_number;
+  for (uint32_t sample_idx = 0;
+     sample_idx < data.size() - multichannel_buffer_size;
+     sample_idx += multichannel_buffer_size) {
+    std::cout << sample_idx << " " << data.size() << std::endl;
+    // process the input buffer
+    float* sample_ptr = data.data() + sample_idx;
 
-  // Run the extraction
-  auto status = bundle->session->Run(
-      {{"Placeholder", tf_input}}, GetOutputNames(separation_type), {}, &output);
-  ASSERT_TRUE(status.ok());
+    buffer.fromInterleaved(sample_ptr);
+    filter.ProcessBlock(&buffer);
+    buffer.toInterleaved(sample_ptr);
+  }
+  
+  
+  wave::File result_file;
+  result_file.Open(std::string(OUTPUT_DIR) + "/result.wav", wave::kOut);
+  result_file.set_sample_rate(44100);
+  result_file.set_channel_number(2);
+  result_file.Write(data);
 }
 
-// void Write(const spleeter::Waveform& data, const std::string& name) {
-//  std::vector<float> vec_data(data.size());
-//  std::copy(data.data(), data.data() + data.size(), vec_data.data());
-//  wave::File file;
-//  file.Open(std::string(OUTPUT_DIR) + "/" + name + ".wav", wave::kOut);
-//  file.set_sample_rate(44100);
-//  file.set_channel_number(2);
-//  file.Write(vec_data);
-//}
-//
-//
 // TEST(Spleeter, TwoStems) {
 //  // Read wav file
 //  wave::File file;
