bb2075df790763d32cbe3532ef85ce7998e26ebc
gvne <guillaume.vincke@gmail.com>
2020-05-26 20:16:22 +0200

    Upload 16KHz models and add the associated cmake option

--- cmake/add_spleeter_models.cmake
+++ cmake/add_spleeter_models.cmake
@@ -10,6 +10,9 @@ set(zip_file models.zip)
 set(zip_path ${spleeter_models_dir}/${zip_file})
 set(expected_sha256 "edfdbdb9b830698fc859afbb9f1e5d8b2a7da1ec33ed1ccc288e9a57ef0c4ca2")
 set(url "https://github.com/gvne/spleeterpp/releases/download/models-1.0/models.zip")
+if (${spleeter_enable_high_resolution})
+  set(url "https://github.com/gvne/spleeterpp/releases/download/models-1.0/models-16KHz.zip")
+endif()
 
 file(MAKE_DIRECTORY ${spleeter_models_dir})
 set(sha256 "")
@@ -18,6 +21,7 @@ if (EXISTS ${zip_path})
 endif()
 
 if (NOT "${sha256}" STREQUAL "${expected_sha256}")
+  message(STATUS "Downloading models from ${url}")
   file(DOWNLOAD ${url} ${zip_path} SHOW_PROGRESS)
   execute_process(
     COMMAND ${CMAKE_COMMAND} -E tar -xf ${zip_file}
@@ -33,6 +37,9 @@ set(zip_file models.zip)
 set(zip_path ${spleeter_filter_models_dir}/${zip_file})
 set(expected_sha256 "3b1f7bc0c495b1b4ecb2c6e43d7bbb561a1fe499bfa16b15e3795687aa287257")
 set(url "https://github.com/gvne/spleeterpp/releases/download/olmodels-v1.0/models.zip")
+if (${spleeter_enable_high_resolution})
+  set(url "https://github.com/gvne/spleeterpp/releases/download/olmodels-v1.0/models-16KHz.zip")
+endif()
 
 file(MAKE_DIRECTORY ${spleeter_filter_models_dir})
 set(sha256 "")
@@ -41,6 +48,7 @@ if (EXISTS ${zip_path})
 endif()
 
 if (NOT "${sha256}" STREQUAL "${expected_sha256}")
+  message(STATUS "Downloading online models from ${url}")
   file(DOWNLOAD ${url} ${zip_path} SHOW_PROGRESS)
   execute_process(
     COMMAND ${CMAKE_COMMAND} -E tar -xf ${zip_file}
